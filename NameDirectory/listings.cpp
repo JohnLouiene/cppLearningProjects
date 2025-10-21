e6d315a66bb8cb3f9db0a025ba4e3d3700bf6e0e
JohnLouiene <johnlouiene@gmail.com>
2025-10-21 22:10:39 +0800

    Updating NameDirectory
    Updating NameDirectory to have a cleaner code and CLI clearing function

--- NameDirectory/listings.cpp
+++ NameDirectory/listings.cpp
@@ -1,59 +1,138 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <cstdlib>
 using namespace std;
 
 /*This Program will have an array of names which we will use to store, edit and delete*/
 
-class Names{
-    public:
-    vector<string> removeFromList(vector<string>& names, string& name){
+void clearScreen()
+{ // Function to clear the CLI everytime we finish an operation
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+class Names
+{
+public:
+    void removeFromList(vector<string> &names, string &name)
+    {
         auto found = find(names.begin(), names.end(), name);
-        if (found != names.end()){
+        if (found != names.end())
+        {
             names.erase(found);
         }
-        else{
-            cout << "Element not found" << endl;
+        else
+        {
+            cout << "Name not found" << endl;
         }
-        return names;
+        return;
     }
 
-    vector<string> addToList(vector<string>& names, string& name){
+    void addToList(vector<string> &names, string &name)
+    {
         names.push_back(name);
-        return names;
+        return;
+    }
+
+    void updateList(vector<string> &names, const string &nameOne, const string &nameTwo)
+    {
+        for (int i = 0; i < names.size(); i++)
+        {
+            if (nameOne == names[i])
+            {
+                names[i] = nameTwo;
+            }
+        }
+        return;
     }
 };
 
-int main(){
+int main()
+{
     Names listOfNames;
     vector<string> names = {};
-    while (true){
+    vector<string> ops = {"add", "remove", "update", "quit"};
+    while (true)
+    {
         string name = "";
+        string name2 = "";
         string operation = "";
-        cout << "Enter a name and operation (add, remove, quit): " << endl;
-        if (!(cin >> name >> operation)){
-            break;
+
+        if (!names.empty())
+        {
+            cout << "Current names: " << endl;
+            for (int i = 0; i < names.size(); i++)
+            {
+                cout << names[i];
+                if (i < names.size() - 1)
+                    cout << ", ";
+            }
+            cout << endl;
         }
-        if (operation == "quit") break;
-        if (operation == "add"){
-            listOfNames.addToList(names, name);
+
+        cout << "Enter an operation (add, remove, update, quit): " << endl;
+        cin >> operation;
+
+        if (operation == "add" || operation == "remove")
+        {
+            cout << "Enter a name: " << endl;
+            if (!(cin >> name))
+            {
+                break;
+            }
+            if (operation == "add")
+            {
+                listOfNames.addToList(names, name);
+            }
+            else if (operation == "remove")
+            {
+                listOfNames.removeFromList(names, name);
+            }
         }
-        else if (operation == "remove"){
-            listOfNames.removeFromList(names, name);
+
+        else if (operation == "update")
+        {
+            cout << "Enter name to edit, then name to update: " << endl;
+            if (!(cin >> name >> name2))
+            {
+                break;
+            }
+            listOfNames.updateList(names, name, name2);
         }
-        else{
-            cout << "Invalid Operation: " << endl;
+
+        else if (operation == "quit")
+        {
             break;
         }
 
-        cout << "Current names: " << endl;
-        for (const string& str : names){
-            cout << " - " << str << endl;
+        else
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "invalid operation, try again" << endl;
         }
 
-        cout << "Press ENTER to continue" << endl;
-        cin.ignore();
+        for (int i = 0; i < ops.size(); i++)
+        {
+            if (operation == ops[i])
+            {
+                cout << "Current names: " << endl;
+                for (const string &str : names)
+                {
+                    cout << " - " << str << endl;
+                }
+            }
+        }
+
+        cout << "Press ENTER twice to continue" << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cin.get();
+        clearScreen();
     }
 
     cout << "Program Terminated" << endl;
